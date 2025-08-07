eb24d4bd39345174ba909502060e45525c0bf128
Arian Grasso <ariangrasso@gmail.com>
2025-08-07 20:53:09 -0300

    modificacion del formato de la fecha

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let personas = [];
 
+const FECHA_ACTUAL = [7, 8, 2025];
+
 const verificadorNumerico = (numero) => {
     while (numero === "" || numero === null || isNaN(numero)) {
         numero = prompt("Ingrese un numero valido");
@@ -11,28 +13,26 @@ const verificadorNumerico = (numero) => {
 let cantidadDePersonas = prompt("Ingrese la cantidad de personas:");
 cantidadDePersonas = verificadorNumerico(cantidadDePersonas);
 
-const agregarPersona = (nombrePersona, fechaPersona) => {
+const agregarPersona = (nombrePersona, dia, mes, año) => {
     personas.push({
         nombre: nombrePersona,
-        nacimiento: fechaPersona,
+        dia: dia,
+        mes: mes,
+        año: año,
     });
 };
 
 const fecha = () => {
-    let dia = prompt("Ingrese dia de nacimiento:");
-    dia = verificadorNumerico(dia);
-    let mes = prompt("Ingrese mes de nacimiento: ");
-    mes = verificadorNumerico(mes);
-    let año = prompt("Ingrese año de nacimiento: ");
-    año = verificadorNumerico(año);
-
-    return `${dia}/${mes}/${año}`;
+    let dia = verificadorNumerico(prompt("Ingrese dia de nacimiento:"));
+    let mes = verificadorNumerico(prompt("Ingrese mes de nacimiento: "));
+    let año = verificadorNumerico(prompt("Ingrese año de nacimiento: "));
+    return [dia, mes, año];
 };
 
 for (let i = 1; i <= cantidadDePersonas; i++) {
     let nombreAgregado = prompt(`Ingrese el nombre de la ${i}º persona:`);
-    let fechaAgregada = fecha();
-    agregarPersona(nombreAgregado, fechaAgregada);
+    let [dia, mes, año] = fecha();
+    agregarPersona(nombreAgregado, dia, mes, año);
 }
 
 const listadoCompleto = () => {
@@ -41,7 +41,7 @@ const listadoCompleto = () => {
     for (let i = 0; i < personas.length; i++) {
         personasTotal += `
         Nombre: ${personas[i].nombre}
-        Fecha de nacimiento: ${personas[i].nacimiento}
+        Fecha de nacimiento: ${personas[i].dia}/${personas[i].mes}/${personas[i].año}
         `;
     }
     return console.log(personasTotal);
